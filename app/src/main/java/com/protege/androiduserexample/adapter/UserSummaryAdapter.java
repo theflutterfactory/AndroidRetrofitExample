a554c2a506c612e0f55b078c5f51f44a89b40673
Julian Currie <julian.currie@gmail.com>
2016-12-05 04:27:28 -0800

    set user summary views in UserSummaryView instead of the adapter

--- app/src/main/java/com/protege/androiduserexample/adapter/UserSummaryAdapter.java
+++ app/src/main/java/com/protege/androiduserexample/adapter/UserSummaryAdapter.java
@@ -1,24 +1,15 @@
 package com.protege.androiduserexample.adapter;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.protege.androiduserexample.R;
-import com.protege.androiduserexample.event.UserSummaryEvent;
 import com.protege.androiduserexample.model.User;
-import com.squareup.picasso.Picasso;
-
-import org.greenrobot.eventbus.EventBus;
+import com.protege.androiduserexample.view.UserSummaryView;
 
 import java.util.ArrayList;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class UserSummaryAdapter extends RecyclerView.Adapter<UserSummaryAdapter.ViewHolder> {
 
@@ -30,21 +21,8 @@ public class UserSummaryAdapter extends RecyclerView.Adapter<UserSummaryAdapter.
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        @BindView(R.id.name)
-        TextView name;
-
-        @BindView(R.id.username)
-        TextView userName;
-
-        @BindView(R.id.company)
-        TextView company;
-
-        @BindView(R.id.profile_photo)
-        ImageView profilePhoto;
-
         public ViewHolder(View view) {
             super(view);
-            ButterKnife.bind(this, view);
         }
     }
 
@@ -57,26 +35,8 @@ public class UserSummaryAdapter extends RecyclerView.Adapter<UserSummaryAdapter.
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Context context = holder.itemView.getContext();
-
         final User user = userList.get(position);
-        holder.name.setText(user.getName());
-        holder.userName.setText(String.format(context.getString(R.string.username),
-                user.getUsername()));
-        holder.company.setText(String.format(context.getString(R.string.company),
-                user.getCompany().getName()));
-
-        //Todo create an interesting placeholder
-        Picasso.with(context)
-                .load("http://bit.ly/2f7pckl")
-                .into(holder.profilePhoto);
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EventBus.getDefault().post(new UserSummaryEvent(user));
-            }
-        });
+        ((UserSummaryView)holder.itemView).setUserSummary(user);
     }
 
     @Override
--- app/src/main/java/com/protege/androiduserexample/view/UserSummaryView.java
+++ app/src/main/java/com/protege/androiduserexample/view/UserSummaryView.java
@@ -3,12 +3,34 @@ package com.protege.androiduserexample.view;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.protege.androiduserexample.R;
+import com.protege.androiduserexample.event.UserSummaryEvent;
 import com.protege.androiduserexample.model.User;
+import com.squareup.picasso.Picasso;
 
+import org.greenrobot.eventbus.EventBus;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class UserSummaryView extends CardView {
+
+    @BindView(R.id.name)
+    TextView name;
+
+    @BindView(R.id.username)
+    TextView userName;
+
+    @BindView(R.id.company)
+    TextView company;
+
+    @BindView(R.id.profile_photo)
+    ImageView profilePhoto;
+
     public UserSummaryView(Context context) {
         super(context);
     }
@@ -23,7 +45,23 @@ public class UserSummaryView extends CardView {
         ButterKnife.bind(this);
     }
 
-    public void setUserSummary(User user) {
+    public void setUserSummary(final User user) {
+        name.setText(user.getName());
+        userName.setText(String.format(getContext().getString(R.string.username),
+                user.getUsername()));
+        company.setText(String.format(getContext().getString(R.string.company),
+                user.getCompany().getName()));
 
+        //Todo create an interesting placeholder
+        Picasso.with(getContext())
+                .load("http://bit.ly/2f7pckl")
+                .into(profilePhoto);
+
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().post(new UserSummaryEvent(user));
+            }
+        });
     }
 }
